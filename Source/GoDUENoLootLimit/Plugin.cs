fa6182db12be15bbffa7d387d02dbc0119736711
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2021-11-22 14:53:16 -0500

    Initial code creation.

---
+++ Source/GoDUENoLootLimit/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace GoDUENoLootLimit
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        public static ManualLogSource Log = null;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is patched!");
+        }
+
+        [HarmonyPatch(typeof(QuestManager), "parseRegion")]
+        [HarmonyPostfix]
+        static public void ParseRegionUnlimitedGoldCap(RegionData rd)
+        {
+            foreach (var adventure in rd.adventures)
+            {
+                foreach (var quest in adventure.quests)
+                {
+                    quest.lootGoldCap = 1000;        //max out loot limit
+                }
+            }
+        }
+    }
+}
